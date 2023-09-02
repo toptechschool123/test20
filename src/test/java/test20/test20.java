c131c33a20fb0f2359229ec3652caf51d0839736
sayed <sayed.sadatibrahim@gmail.com>
2023-09-01 18:45:09 -0700

    msge2

--- src/test/java/test20/test20.java
+++ src/test/java/test20/test20.java
@@ -5,6 +5,10 @@ public class test20 {
 	public static void main(String[] args) {
 
 		System.out.println("it is test 20 project");
+		
+		System.out.println("it is test 20 project");
+		
+		System.out.println("it is test 20 project");
 
 	}
 
